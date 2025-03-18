cfb9d7feb8eee5f32747fe306a6bd8a6435dc1cc
Kim Daseul <osl123o@naver.com>
2025-03-18 20:22:09 +0900

    03장 스프링 부트 이해하기

--- springboot-developer/src/main/java/me/dev/springbootdeveloper/TestContoller.java
+++ springboot-developer/src/main/java/me/dev/springbootdeveloper/TestContoller.java
@@ -1,12 +1,21 @@
 package me.dev.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+import java.util.List;
+
+@RestController //라우터 역활을 하는 애너테이션
 public class TestContoller {
+    @Autowired
+    TestService testService;
+
     @GetMapping("/test")
-    public String test(){
-        return "Hello, world!";
+    public List<Member> getAllMembers(){
+        List<Member> members;
+        members = testService.getAllMembers();
+        return members;
     }
 }
